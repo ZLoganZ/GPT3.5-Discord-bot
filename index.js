1344096951bcdecdf0bcd5c4964b880f4f95cd03
ZLoganZ <ln26805@gmail.com>
2023-09-13 21:46:37 +0700

    z

--- index.js
+++ index.js
@@ -108,3 +108,9 @@ client.on('messageCreate', async (message) => {
 });
 
 client.login(process.env.TOKEN);
+
+const server = require('http').createServer();
+const port = process.env.PORT || 3000;
+server.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
